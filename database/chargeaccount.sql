6873ab41ac704bc514fe5e7bf7ea733c2f7d4589
geekchen <cscbeyond@sina.com>
2019-10-20 22:09:49 +0800

    数据库表结构

---
+++ database/chargeaccount.sql
@@ -0,0 +1,67 @@
+/*
+ Navicat Premium Data Transfer
+
+ Source Server         : localhost
+ Source Server Type    : MySQL
+ Source Server Version : 50727
+ Source Host           : localhost:3306
+ Source Schema         : chargeaccount
+
+ Target Server Type    : MySQL
+ Target Server Version : 50727
+ File Encoding         : 65001
+
+ Date: 20/10/2019 22:09:14
+*/
+
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for account_record
+-- ----------------------------
+DROP TABLE IF EXISTS `account_record`;
+CREATE TABLE `account_record` (
+  `openid` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `item_name` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `count` bigint(16) DEFAULT NULL,
+  `create_time` datetime DEFAULT NULL,
+  `total_days` int(16) DEFAULT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `type` int(6) NOT NULL,
+  `icon` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `week` int(6) NOT NULL,
+  `des` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE=InnoDB AUTO_INCREMENT=272 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- ----------------------------
+-- Table structure for icons
+-- ----------------------------
+DROP TABLE IF EXISTS `icons`;
+CREATE TABLE `icons` (
+  `id` int(6) NOT NULL AUTO_INCREMENT,
+  `icon` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `des` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `openid` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `type` int(6) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=74 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- ----------------------------
+-- Table structure for user
+-- ----------------------------
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE `user` (
+  `id` int(16) NOT NULL AUTO_INCREMENT,
+  `openid` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `create_time` datetime NOT NULL,
+  `mobile` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `nickname` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `pwd` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `avatarUrl` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `last_login_time` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=160 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+SET FOREIGN_KEY_CHECKS = 1;
